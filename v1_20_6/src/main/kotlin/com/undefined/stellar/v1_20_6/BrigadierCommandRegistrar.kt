b545f63113476f3d514ebeb4ff3790558c6b61a8
StillLutto <still.lutto@gmail.com>
2024-10-26 14:31:11 +0200

    fix(brigadier): aliases not being handled correctly

--- v1_20_6/src/main/kotlin/com/undefined/stellar/v1_20_6/BrigadierCommandRegistrar.kt
+++ v1_20_6/src/main/kotlin/com/undefined/stellar/v1_20_6/BrigadierCommandRegistrar.kt
@@ -3,7 +3,6 @@ package com.undefined.stellar.v1_20_6
 import com.mojang.brigadier.CommandDispatcher
 import com.mojang.brigadier.builder.LiteralArgumentBuilder
 import com.undefined.stellar.BaseStellarCommand
-import com.undefined.stellar.v1_20_6.BrigadierCommandRegistrar.handleAliases
 import net.minecraft.commands.CommandSourceStack
 import org.bukkit.Bukkit
 import org.bukkit.craftbukkit.CraftServer
@@ -11,24 +10,14 @@ import org.bukkit.craftbukkit.CraftServer
 object BrigadierCommandRegistrar {
 
     fun register(stellarCommand: BaseStellarCommand) {
-        val mainArgumentBuilder = LiteralArgumentBuilder.literal<CommandSourceStack>(stellarCommand.name)
-        mainArgumentBuilder.handleCommand(stellarCommand)
-        getCommandDispatcher().register(mainArgumentBuilder)
+        stellarCommand.aliases.plus(stellarCommand.name).forEach { alias ->
+            val mainArgumentBuilder = LiteralArgumentBuilder.literal<CommandSourceStack>(alias)
+            mainArgumentBuilder.handleCommand(stellarCommand)
+            getCommandDispatcher().register(mainArgumentBuilder)
+        }
     }
 
     private fun LiteralArgumentBuilder<CommandSourceStack>.handleCommand(stellarCommand: BaseStellarCommand) {
-        stellarCommand.aliases.forEach { alias ->
-            println(stellarCommand.name)
-            println(alias)
-            val childArgumentBuilder = LiteralArgumentBuilder.literal<CommandSourceStack>(alias)
-            childArgumentBuilder.handleCommandWithoutAliases(stellarCommand)
-            this.then(childArgumentBuilder)
-        }
-//        handleAliases(stellarCommand)
-        handleCommandWithoutAliases(stellarCommand)
-    }
-
-    private fun LiteralArgumentBuilder<CommandSourceStack>.handleCommandWithoutAliases(stellarCommand: BaseStellarCommand) {
         handleSubCommands(stellarCommand)
         handleRequirements(stellarCommand)
         handleExecutions(stellarCommand)
@@ -37,7 +26,7 @@ object BrigadierCommandRegistrar {
     private fun LiteralArgumentBuilder<CommandSourceStack>.handleAliases(stellarCommand: BaseStellarCommand) {
         stellarCommand.aliases.forEach { alias ->
             val childArgumentBuilder = LiteralArgumentBuilder.literal<CommandSourceStack>(alias)
-            childArgumentBuilder.handleCommandWithoutAliases(stellarCommand)
+            childArgumentBuilder.handleCommand(stellarCommand)
             this.then(childArgumentBuilder)
         }
     }
@@ -46,6 +35,7 @@ object BrigadierCommandRegistrar {
         stellarCommand.subCommands.forEach { subCommand ->
             val subCommandArgument = LiteralArgumentBuilder.literal<CommandSourceStack>(subCommand.name)
             subCommandArgument.handleCommand(subCommand)
+            this.handleAliases(subCommand)
             then(subCommandArgument)
         }
     }
