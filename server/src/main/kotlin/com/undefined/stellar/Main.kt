8d8f868212e5c9317e517ff6336b4d7d62077548
StillLutto <still.lutto@gmail.com>
2024-12-17 14:33:55 +0100

    feat(class-commands): add class commands

--- server/src/main/kotlin/com/undefined/stellar/Main.kt
+++ server/src/main/kotlin/com/undefined/stellar/Main.kt
@@ -1,14 +1,33 @@
 package com.undefined.stellar
 
+import com.undefined.stellar.argument.types.misc.UUIDArgument
+import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
+
+class TestCommand : BaseStellarCommand("test", "description") {
+    override fun setup() = createCommand {
+        addExecution<Player> {
+            sender.sendMessage("hi!")
+        }
+    }
+
+    override fun arguments(): List<StellarArgument> = listOf(TestArgument(this.command))
+}
+
+class TestArgument(parent: AbstractStellarCommand<*>) : StellarArgument(UUIDArgument(parent, "sub")) {
+    override fun setup() = createArgument {
+        addExecution<Player> {
+            sender.sendMessage("Hello there! Argument.")
+        }
+    }
+}
 
 class Main : JavaPlugin() {
 
     override fun onEnable() {
-        val main = StellarCommand("test")
-        main.addGreedyStringArgument("args")
-            .addWordSuggestions(1, "test")
-            .register(this)
+        val command = TestCommand()
+        command.register(this)
+        println(command.name)
     }
 
 }
