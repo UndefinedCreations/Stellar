c2b3ea5e3b662c1ea884f1a4f8e943f1c8ef3360
StillLutto <still.lutto@gmail.com>
2025-03-11 00:32:09 +0100

    feat(recode): add docs to StellarCommand

--- server/src/main/kotlin/com/undefined/stellar/Main.kt
+++ server/src/main/kotlin/com/undefined/stellar/Main.kt
@@ -1,21 +1,16 @@
 package com.undefined.stellar
 
-import com.undefined.stellar.argument.list.OnlinePlayersArgument
 import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
 
     override fun onEnable() {
-        StellarCommand("test")
-            .addArgument(OnlinePlayersArgument("test"))
+        StellarCommand("test", listOf("test"))
+            .addStringArgument("test")
             .addExecution<Player> {
-                sender.sendMessage(getArgument<Player>("test").name)
+                sender.sendMessage(getArgument<String>("test"))
             }
-//            .addArgument(RegistryArgument("registry", Registry.CAT_VARIANT))
-//            .addExecution<Player> {
-//                sender.sendMessage(getArgument<Cat.Type>("registry").key.toString())
-//            }
             .register(this)
     }
 
--- spigot/api/src/main/kotlin/com/undefined/stellar/StellarCommand.kt
+++ spigot/api/src/main/kotlin/com/undefined/stellar/StellarCommand.kt
@@ -3,11 +3,31 @@ package com.undefined.stellar
 import org.bukkit.plugin.java.JavaPlugin
 import java.util.*
 
+/**
+ * This is the base of any command, e.g. /op <target>, op would be the base command.
+ *
+ * @param name The name of the command.
+ * @param permissions An `Iterable` of Bukkit string permissions to be applied as a requirement.
+ * @param aliases An `Iterable` of name aliases.
+ */
 class StellarCommand(name: String, permissions: Iterable<String> = listOf(), aliases: Iterable<String> = listOf()) : AbstractStellarCommand<StellarCommand>(name) {
 
     val information: SortedMap<String, String> = sortedMapOf()
 
+    /**
+     * This is the base of any command, e.g. /op <target>, op would be the base command.
+     *
+     * @param name The name of the command.
+     * @param aliases An `Iterable` of name aliases.
+     */
     constructor(name: String, aliases: Iterable<String>) : this(name, permissions = listOf(), aliases = aliases)
+    /**
+     * This is the base of any command, e.g. /op <target>, op would be the base command.
+     *
+     * @param name The name of the command.
+     * @param permission A single Bukkit permission to be applied as a requirement.
+     * @param aliases An `Iterable` of name aliases.
+     */
     constructor(name: String, permission: String, aliases: Iterable<String> = listOf()) : this(name, listOf(permission), aliases)
 
     init {
