849e5dbacebc1370fea05eeb8b35c9864d957c6d
StillLutto <still.lutto@gmail.com>
2025-03-11 23:00:05 +0100

    refactor(recode): add class

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,19 @@
 plugins {
     id("setup")
+    `publishing-convention`
+    id("com.gradleup.shadow") version "8.3.5"
+}
+
+dependencies {
+    compileOnly(libs.spigot)
+    compileOnly(libs.brigadier)
+    api(project(":spigot:lib"))
+    api(project(":spigot:v1_21_4"))
+}
+
+tasks {
+    shadowJar {
+        exclude("**/kotlin/**")
+        archiveClassifier = ""
+    }
 }
---
+++ paper/api/src/main/kotlin/com/undefined/stellar/StellarArgument.kt
@@ -0,0 +1,23 @@
+package com.undefined.stellar
+
+abstract class StellarArgument(val type: AbstractStellarArgument<*, *>, val permissions: List<String>) {
+
+    constructor(type: AbstractStellarArgument<*, *>, vararg permissions: String) : this(type, permissions.toList())
+
+    val argument: AbstractStellarArgument<*, *> by lazy {
+        setup().apply { addRequirements(*permissions.toTypedArray()) }
+    }
+
+    abstract fun setup(): AbstractStellarArgument<*, *>
+    open fun arguments(): List<StellarArgument> = listOf()
+
+    fun createArgument(init: AbstractStellarArgument<*, *>.() -> Unit): AbstractStellarArgument<*, *> = type.apply { init() }
+
+    fun getFullArgument(): AbstractStellarArgument<*, *> {
+        val command = setup()
+        command.addRequirements(*permissions.toTypedArray())
+        for (argument in arguments()) command.addArgument(argument.getFullArgument())
+        return command
+    }
+
+}
