ad18f35b7839d73ffb83d8e7c24f2f5fb59db323
StillLutto <still.lutto@gmail.com>
2025-03-20 20:34:34 +0100

    feat(docs): add docs to exceptions

--- common/src/main/kotlin/com/undefined/stellar/exception/UnsupportedVersionException.kt
+++ common/src/main/kotlin/com/undefined/stellar/exception/UnsupportedVersionException.kt
@@ -1,3 +1,6 @@
 package com.undefined.stellar.exception
 
-class UnsupportedVersionException(supportedVersions: Collection<String>) : Exception("This minecraft version is unsupported! Supported versions: $supportedVersions")
+/**
+ * This exception is thrown whenever this API does not support a feature or entirely on a specific Minecraft version.
+ */
+class UnsupportedVersionException(supportedVersions: Collection<String>) : Exception("This Minecraft version is unsupported! Supported versions: $supportedVersions")
