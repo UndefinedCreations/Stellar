b2aa2c246c965e612d2aed0919793964ce1f33a4
StillLutto <still.lutto@gmail.com>
2025-03-21 11:45:01 +0100

    feat(docs): add docs to data/execution

--- common/src/main/kotlin/com/undefined/stellar/data/execution/ExecutableRunnable.kt
+++ common/src/main/kotlin/com/undefined/stellar/data/execution/ExecutableRunnable.kt
@@ -2,10 +2,12 @@ package com.undefined.stellar.data.execution
 
 import com.undefined.stellar.data.argument.CommandContext
 import org.bukkit.command.CommandSender
+import org.jetbrains.annotations.ApiStatus
 import kotlin.reflect.KClass
 import kotlin.reflect.safeCast
 
 @Suppress("UNCHECKED_CAST")
+@ApiStatus.Internal
 data class ExecutableRunnable<C : CommandSender>(val clazz: KClass<C>, val runnable: StellarRunnable<C>, val async: Boolean) {
     operator fun invoke(context: CommandContext<CommandSender>): Boolean {
         if (clazz.safeCast(context.sender) == null) return false
--- common/src/main/kotlin/com/undefined/stellar/data/execution/StellarExecution.kt
+++ common/src/main/kotlin/com/undefined/stellar/data/execution/StellarExecution.kt
@@ -3,6 +3,9 @@ package com.undefined.stellar.data.execution
 import com.undefined.stellar.data.argument.CommandContext
 import org.bukkit.command.CommandSender
 
+/**
+ * Represents a functional interface used for command executions.
+ */
 fun interface StellarExecution<C : CommandSender> {
     operator fun invoke(context: CommandContext<C>)
 }
