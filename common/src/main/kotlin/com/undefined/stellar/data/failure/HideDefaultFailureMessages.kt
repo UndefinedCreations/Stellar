39168318b25ebe57d97f89332b38128a2b8ef4f3
StillLutto <still.lutto@gmail.com>
2025-03-21 11:47:03 +0100

    feat(docs): add docs to data/failure

--- common/src/main/kotlin/com/undefined/stellar/data/failure/HideDefaultFailureMessages.kt
+++ common/src/main/kotlin/com/undefined/stellar/data/failure/HideDefaultFailureMessages.kt
@@ -1,3 +1,9 @@
 package com.undefined.stellar.data.failure
 
+/**
+ * A data class with the possibility to hide the default Minecraft failure messages.
+ *
+ * @param hide Whether to hide the default failure messages.
+ * @param global Whether it should also be applied for all subsequent arguments.
+ */
 data class HideDefaultFailureMessages(val hide: Boolean, val global: Boolean)
