f462469eae49d533fd16676efbb52ea2e137f4fd
StillLutto <still.lutto@gmail.com>
2025-03-23 14:02:12 +0100

    feat(docs): setup dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,25 @@
+import org.jetbrains.dokka.gradle.DokkaTask
+
 plugins {
     id("setup")
     `publishing-convention`
     id("com.gradleup.shadow") version "8.3.5"
+    id("org.jetbrains.dokka") version "2.0.0"
 }
 
 dependencies {
     compileOnly(libs.spigot)
     compileOnly(libs.brigadier)
+
     api(project(":paper:lib"))
     api(project(":spigot:lib"))
+
+    dokkaPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:2.0.0")
+}
+
+subprojects {
+    apply(plugin = "org.jetbrains.dokka")
+    tasks.withType<DokkaTask>()
 }
 
 tasks {
