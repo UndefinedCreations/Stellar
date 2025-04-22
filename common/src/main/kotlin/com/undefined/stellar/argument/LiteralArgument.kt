c641a7b354bcbd3d00cf903f257b7ce49850371a
StillLutto <still.lutto@gmail.com>
2025-04-22 13:54:51 +0200

    feat(docs): add docs to literal argument and improve docs for UnsupportedArgumentException

--- common/src/main/kotlin/com/undefined/stellar/argument/LiteralArgument.kt
+++ common/src/main/kotlin/com/undefined/stellar/argument/LiteralArgument.kt
@@ -2,4 +2,9 @@ package com.undefined.stellar.argument
 
 import com.undefined.stellar.AbstractStellarArgument
 
+/**
+ * An argument that represents an option you can choose from.
+ *
+ * You cannot get the parsed value of this argument, if you try, you will get a [].
+ */
 class LiteralArgument(name: String) : AbstractStellarArgument<LiteralArgument>(name)
--- common/src/main/kotlin/com/undefined/stellar/data/exception/UnsupportedArgumentException.kt
+++ common/src/main/kotlin/com/undefined/stellar/data/exception/UnsupportedArgumentException.kt
@@ -3,7 +3,6 @@ package com.undefined.stellar.data.exception
 import com.undefined.stellar.ParameterArgument
 
 /**
- * This exception is thrown whenever an argument is used that is not fully implemented by the Stellar API.
- * This is not supposed to happen.
+ * This exception is thrown whenever an argument is used that is not supported for this specific version or not fully implemented by the Stellar API.
  */
-class UnsupportedArgumentException(val argument: ParameterArgument<*, *>) : RuntimeException("${argument::class.simpleName} is unsupported by Stellar! This is entirely unintentional behaviour.")
+class UnsupportedArgumentException(val argument: ParameterArgument<*, *>) : RuntimeException("${argument::class.simpleName} is unsupported by Stellar! Check if the argument you are using supports this Minecraft server version.")
