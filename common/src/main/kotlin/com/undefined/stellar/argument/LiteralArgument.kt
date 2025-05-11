084192666e5bc1f4856805461d56eb1a77a6b740
StillLutto <still.lutto@gmail.com>
2025-05-11 11:50:08 +0200

    fix(literal-argument): improve docs

--- common/src/main/kotlin/com/undefined/stellar/argument/LiteralArgument.kt
+++ common/src/main/kotlin/com/undefined/stellar/argument/LiteralArgument.kt
@@ -5,6 +5,6 @@ import com.undefined.stellar.AbstractStellarArgument
 /**
  * An argument that represents an option you can choose from.
  *
- * You cannot get the parsed value of this argument, if you try, you will get a [].
+ * You cannot get the parsed value of this argument.
  */
 class LiteralArgument(name: String) : AbstractStellarArgument<LiteralArgument>(name)
