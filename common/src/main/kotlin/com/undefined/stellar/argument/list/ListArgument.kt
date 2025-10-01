38f79d9203aea6038166fcfd6ff949a2ef7fa7e5
StillLutto <still.lutto@gmail.com>
2025-10-01 17:45:24 +0200

    feat: improve list argument

--- common/src/main/kotlin/com/undefined/stellar/argument/list/ListArgument.kt
+++ common/src/main/kotlin/com/undefined/stellar/argument/list/ListArgument.kt
@@ -6,23 +6,43 @@ import com.undefined.stellar.data.exception.UnsupportedArgumentTypeException
 import com.undefined.stellar.data.suggestion.Suggestion
 import org.bukkit.command.CommandSender
 import org.jetbrains.annotations.ApiStatus
-import org.jetbrains.annotations.ApiStatus.OverrideOnly
 import java.util.concurrent.CompletableFuture
+import kotlin.collections.mapNotNull
 
 /**
  * An argument that wraps around an [ParameterArgument] and adds a suggestion based on the list provided.
  *
  * @param base The [ParameterArgument] it wraps around.
- * @param list A function providing a [CommandContext] and returning the list used in the argument in a [CompletableFuture].
- * @param converter A function providing a [CommandSender] and a value from the list, returning the [Suggestion] shown in-game. This is used to convert each element in the list ot a suggestion viewable in-game (default: uses `toString()`).
+ * @param list A function providing a [CommandContext] and returning the list of [Suggestion] used in the command.
  * @param parse A function providing a [CommandSender] and the value returned from the [ParameterArgument], returning the argument parsed from the [base] argument.
  */
 open class ListArgument<T, R>(
 	val base: ParameterArgument<*, R>,
-	val list: CommandContext<CommandSender>.() -> CompletableFuture<Iterable<T>>,
-	val converter: CommandSender.(T) -> Suggestion? = { Suggestion.withText(it.toString()) },
+	val list: CommandContext<CommandSender>.(input: String) -> CompletableFuture<Iterable<Suggestion>>,
 	val parse: CommandSender.(R) -> T?,
 ) : ParameterArgument<ListArgument<T, R>, R>(base.name, base.argumentType) {
+
+	/**
+	 * An argument that wraps around an [ParameterArgument] and adds a suggestion based on the list provided.
+	 *
+	 * @param base The [ParameterArgument] it wraps around.
+	 * @param list A function providing a [CommandContext] and returning the list used in the argument in a [CompletableFuture].
+	 * @param converter A function providing a [CommandSender] and a value from the list, returning the [Suggestion] shown in-game. This is used to convert each element in the list ot a suggestion viewable in-game (default: uses `toString()`).
+	 * @param parse A function providing a [CommandSender] and the value returned from the [ParameterArgument], returning the argument parsed from the [base] argument.
+	 */
+	@JvmOverloads
+	constructor(
+		base: ParameterArgument<*, R>,
+		list: CommandContext<CommandSender>.() -> CompletableFuture<Iterable<T>>,
+		converter: CommandSender.(T) -> Suggestion? = { Suggestion.withText(it.toString()) },
+		parse: CommandSender.(R) -> T?,
+	) : this(base, list = { input ->
+		list().thenApply { list ->
+			list.mapNotNull {
+				converter(sender, it).takeIf { suggestion -> suggestion?.text?.isNotBlank() == true }
+			}.filter { it.text.startsWith(input, true) && it.text != input }
+		}
+	}, parse)
 
 	/**
 	 * An argument that wraps around an [ParameterArgument] and adds a suggestion based on the list provided.
@@ -66,31 +86,13 @@ open class ListArgument<T, R>(
 	init {
 		if (base is ListArgument<*, *>) throw UnsupportedArgumentTypeException(base)
 		addFutureSuggestion<CommandSender> { input ->
-			getSuggestionList(this, input)
+			list(input)
 		}
 	}
 
 	@Suppress("UNCHECKED_CAST")
 	@ApiStatus.Internal
 	fun parseInternal(sender: CommandSender, value: Any): T? = parse(sender, value as R)
-
-	/**
-	 * An open function that returns the list of suggestions used in the argument.
-	 *
-	 * Note: This method should only be overridden! This is not meant to be called from the end user.
-	 */
-	@OverrideOnly
-	open fun getSuggestionList(context: CommandContext<CommandSender>, input: String): CompletableFuture<Iterable<Suggestion>> =
-		list(context).thenApply { list ->
-			println("ListArgument.getSuggestionList")
-			println("list: ${list}")
-			println("list: ${list.mapNotNull {
-				converter(context.sender, it)
-			}}")
-			list.mapNotNull {
-				converter(context.sender, it).takeIf { it?.text?.isNotBlank() == true }
-			}.filter { it.text.startsWith(input, true) && it.text != input }
-		}
 
 	companion object {
 		/**
