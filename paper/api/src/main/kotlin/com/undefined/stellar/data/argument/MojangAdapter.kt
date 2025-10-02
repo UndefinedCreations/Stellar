27c5086371c1735cb52272e9ad3b802a5cc601cd
StillLutto <still.lutto@gmail.com>
2025-10-02 19:09:05 +0200

    feat(list): improve list exception for papermc

--- paper/api/src/main/kotlin/com/undefined/stellar/data/argument/MojangAdapter.kt
+++ paper/api/src/main/kotlin/com/undefined/stellar/data/argument/MojangAdapter.kt
@@ -1,12 +1,14 @@
 package com.undefined.stellar.data.argument
 
-import com.mojang.brigadier.exceptions.CommandSyntaxException
-import com.mojang.brigadier.exceptions.SimpleCommandExceptionType
+import com.mojang.brigadier.exceptions.DynamicCommandExceptionType
 import com.undefined.stellar.*
 import com.undefined.stellar.argument.LiteralArgument
 import com.undefined.stellar.argument.list.ListArgument
 import com.undefined.stellar.data.exception.DuplicateArgumentNameException
 import com.undefined.stellar.nms.NMSHelper
+import io.papermc.paper.command.brigadier.MessageComponentSerializer
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.command.CommandSender
 import org.jetbrains.annotations.ApiStatus
 import com.mojang.brigadier.context.CommandContext as BrigadierCommandContext
@@ -14,7 +16,14 @@ import com.mojang.brigadier.context.CommandContext as BrigadierCommandContext
 @ApiStatus.Internal
 object MojangAdapter {
 
-    private val INVALID_LIST_ENTRY = SimpleCommandExceptionType { "Invalid list entry!" }
+    private val UNKNOWN_COMMAND = DynamicCommandExceptionType { msg ->
+        val message: String = msg as? String ?: throw error("Message must be string!")
+        val component = Component.translatable("command.unknown.command")
+            .appendNewline()
+            .append(Component.text("... $message").color(NamedTextColor.GRAY))
+            .append(Component.translatable("command.context.here"))
+        MessageComponentSerializer.message().serialize(component).also { println("message: $it") }
+    }
 
     fun getStellarCommandContext(context: BrigadierCommandContext<Any>): CommandContext<CommandSender> {
         val input = context.input.removePrefix("/")
@@ -28,10 +37,14 @@ object MojangAdapter {
         val parsedArguments: CommandTree =
             arguments.associate<AbstractStellarArgument<*>, String, (CommandContext<CommandSender>) -> Any> { argument ->
                 Pair(argument.name) {
-                    println("parsing: ${argument.name}")
-                    if (argument is ListArgument<*, *>)
-                        return@Pair argument.parseInternal(sender, NMSManager.nms.parseArgument(context, argument.base) ?: context.getArgument(argument.name, Any::class.java))
-                            ?: throw CommandSyntaxException.BUILT_IN_EXCEPTIONS.dispatcherParseException().create(input)
+                    if (argument is ListArgument<*, *>) {
+                        val argumentInput = NMSHelper.getArgumentInput(context, argument.name) ?: error("Could not get argument input (${argument.name})")
+
+                        return@Pair argument.parseInternal(
+                            sender,
+                            NMSManager.nms.parseArgument(context, argument.base) ?: context.getArgument(argument.name, Any::class.java)
+                        ) ?: throw UNKNOWN_COMMAND.create(argumentInput)
+                    }
                     (NMSManager.nms.parseArgument(context, argument as ParameterArgument<*, *>) ?: context.getArgument(argument.name, Any::class.java)) ?: throw IllegalArgumentException("Argument return value cannot be null!")
                 }
             } as CommandTree
